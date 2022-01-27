c8b06370bd87758236320ac2f29bdb321f20c965
luftedar <ugurorcun91@gmail.com>
2022-01-27 13:33:08 +0300

    Set an instance of Corrector in Person on initialization

--- person.rb
+++ person.rb
@@ -1,9 +1,12 @@
+require "./corrector.rb"
+
 class Person
   def initialize(age, name = 'unknown', parent_permission: true)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @corrector = Corrector.new()
   end
 
   attr_reader :id
