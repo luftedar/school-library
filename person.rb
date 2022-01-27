0fdc4ea6a30b5cf2519d31bd0a90fd84bbd3f1a1
luftedar <ugurorcun91@gmail.com>
2022-01-27 16:06:17 +0300

    Fixed Linter Errors

--- person.rb
+++ person.rb
@@ -3,6 +3,7 @@ require './corrector'
 class Person
   attr_reader :id
   attr_accessor :name, :age, :rentals, :parent_permission
+
   def initialize(age, name = 'unknown', parent_permission: true)
     @id = Random.rand(1..1000)
     @name = name
