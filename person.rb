0c449cc8fe842952d5a0440a7c0b2d3fb4968853
luftedar <ugurorcun91@gmail.com>
2022-01-28 14:14:26 +0300

    Fix classroom.rb

--- person.rb
+++ person.rb
@@ -5,7 +5,7 @@ class Person
   attr_reader :id
   attr_accessor :name, :age, :rentals, :parent_permission
 
-  def initialize(age, name = 'unknown', parent_permission: true)
+  def initialize(age:, name: 'unknown', parent_permission: true)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
