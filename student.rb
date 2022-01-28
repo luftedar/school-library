ec1676d8fda3a77d1a43605e06f8b91f6403cf77
luftedar <ugurorcun91@gmail.com>
2022-01-28 14:43:53 +0300

    Resolve student.rb

--- student.rb
+++ student.rb
@@ -1,19 +1,19 @@
-require_relative 'person'
+equire_relative 'person'
 
 class Student < Person
   attr_reader :classroom
 
-  def initialize(classroom:, age:, name: 'Unknown', parent_permission: true)
-    super(age: age, name: name, parent_permission: parent_permission)
+  def initialize(age:, classroom:, name: 'Unknown', parent_permission: true)
+    super(name: name, age: age, parent_permission: parent_permission)
     @classroom = classroom
-  end
-
-  def play_hooky
-    "¯\(ツ)/¯"
   end
 
   def classroom=(classroom)
     @classroom = classroom
     classroom.students.push(self) unless classroom.students.include?(self)
+  end
+
+  def play_hooky
+    "¯\(ツ)/¯"
   end
 end
