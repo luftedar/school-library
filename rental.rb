f34570edce11b4e62c77eda5b2a01beba8ad4d1a
luftedar <ugurorcun91@gmail.com>
2022-01-28 14:43:29 +0300

    Resolve rental.rb

--- rental.rb
+++ rental.rb
@@ -3,9 +3,9 @@ class Rental
 
   def initialize(date, book, person)
     @date = date
-    @person = person
-    person.rentals << self
     @book = book
     book.rentals << self
+    @person = person
+    person.rentals << self
   end
 end
